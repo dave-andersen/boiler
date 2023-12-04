421403e65d4c330f3f9e99f27a27d0d553417423
Dave Andersen <dga@cs.cmu.edu>
2023-12-03 20:57:24 -0500

    initial import

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # boiler
 Monitoring and controlling Dave's boiler via modbus
+
+Currently just a script to log boiler data every minute to json
---
+++ python/boiler.py
@@ -0,0 +1,85 @@
+import asyncio
+import json
+import pymodbus
+import pymodbus.client
+import datetime
+import time
+
+datafields = []
+
+SERIAL="/dev/serial0"
+DEVICE = 250
+
+
+class DataField:
+    def __init__(self, name, address, length, data_type, degree_wrap=False):
+        self.name = name
+        self.address = address
+        self.length = length
+        self.data_type = data_type
+        self.degree_wrap = degree_wrap
+        global datafields
+        datafields.append(self)
+
+
+df_output_temp = DataField("output_temp", 0x9106, 1, "uint8")
+df_boiler_target_temp = DataField("boiler_target_temp", 0x9109, 1, "uint8")
+df_boiler_status = DataField("boiler status", 0x9105, 1, "uint8")
+df_outdoor_temp = DataField("outdoor_temp", 0x9112, 1, "uint8", degree_wrap=True) # note negative handling
+df_config_supply_max = DataField("supply_max", 0x9120, 1, "uint8")
+df_config_supply_min = DataField("supply_min", 0x9121, 1, "uint8")
+df_config_odr_max = DataField("odr_max", 0x9122, 1, "uint8")
+df_config_odr_min = DataField("odr_min", 0x9123, 1, "uint8", degree_wrap=True)
+df_config_boiler_max = DataField("boiler_max", 0x9124, 1, "uint8")
+df_boier_out_1 = DataField("boiler_out_1_temp", 0x9180, 1, "uint8")
+df_boiler_in_temp = DataField("boiler_in_temp", 0x9188, 1, "uint8")
+df_input_status = DataField("input_status", 0x918B, 1, "uint8")
+df_flue_temp = DataField("flue_temp_1", 0x918C, 1, "uint8")
+df_supply_temp = DataField("local_supply_temp", 0x918D, 1, "uint8")
+df_return_temp = DataField("local_return_temp", 0x9196, 1, "uint8")
+df_modulation_rate = DataField("boiler_modulation_rate", 0x9232, 1, "uint8")
+
+
+
+async def read_datafield(client, datafield):
+    rr = await client.read_holding_registers(
+        datafield.address, datafield.length, slave=DEVICE
+    )
+    #print(f"{datafield.name}: ", rr.registers)
+    v = rr.registers[0]
+    if datafield.degree_wrap and v > 232:
+        v = v - 256
+    return v
+
+
+async def main():
+    with open("log.json", "a") as f:
+      while True:
+        client = pymodbus.client.AsyncModbusSerialClient(
+            port=SERIAL, baudrate=19200, stopbits=1, parity="N"
+        )
+    
+        await client.connect()
+        assert client.connected
+
+        #outdoor_temp = await read_datafield(client, df_outdoor_temp)
+        #boiler_temp = await read_datafield(client, df_output_temp)
+        #boiler_status = await read_datafield(client, df_boiler_status)
+        #config = [await read_datafield(client, field) for field in [df_config_supply_max, df_config_supply_min, df_config_odr_max, df_config_odr_min]]
+        #boiler_on = boiler_status & 0x01
+        #boiler_on = "on" if boiler_on else "off"
+        #print(f"outdoor temp: {outdoor_temp} boiler temp: {boiler_temp} boiler is {boiler_on}")
+        #print("Config", config)
+        status = { df.name : await read_datafield(client, df) for df in datafields }
+        status["time"] = datetime.datetime.utcnow().replace(microsecond=0).isoformat()
+        json.dump(status, f)
+        f.write("\n")
+        f.flush()
+    
+
+        client.close()
+        time.sleep(60)
+
+
+asyncio.run(main())
+
