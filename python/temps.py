4cad671deb87d6b2e63fc51403286eccf849749a
David G. Andersen <dga@cs.cmu.edu>
2023-12-11 16:42:49 -0500

    New temps config

--- python/temps.py
+++ python/temps.py
@@ -6,11 +6,8 @@ class Temps:
         self.supply_max = supply_max
 
 
-conf_old = Temps(-13, 68, 86, 165)
-conf_new = Temps(-14, 67, 85, 168)
-conf_new2 = Temps(-14, 66, 85, 169)
-conf_new3 = Temps(-14, 64, 85, 171)
-conf_new4 = Temps(-14, 63, 85, 175)
+conf_old = Temps(-14, 63, 85, 175)
+conf_new = Temps(-14, 50, 101, 175)
 
 
 def f(t, c):
@@ -22,7 +19,7 @@ def f(t, c):
     return c.supply_min + percent * (c.supply_max - c.supply_min)
 
 
-for c in [conf_old, conf_new, conf_new2, conf_new3, conf_new4]:
+for c in [conf_old, conf_new]:
     for degree in [49, 44, 41, 39, 29, 19]:
         print(degree, f(degree, c))
     print()
