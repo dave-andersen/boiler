74bd8a71c7f75930dd0d5b4867ad7ef1bb5c003c
Dave Andersen <dga@cs.cmu.edu>
2023-12-12 09:13:39 -0500

    allow splitting of logfiles

--- python/analyze.py
+++ python/analyze.py
@@ -1,3 +1,4 @@
+import argparse
 import datetime
 import json
 
@@ -17,6 +18,17 @@ return_temps = []
 BOILER_BTU = 155000
 
 
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--num_days", default=1, type=int, help="number of days back to plot"
+    )
+    return parser.parse_args()
+
+
+args = parse_args()
+
+
 def btu_to_mcf(btu):
     return btu / 1039000.0
 
@@ -31,70 +43,73 @@ state = 0
 last_start = None
 last_stop = None
 
-with open("log.json") as f:
-    for line in f:
-        if not line.startswith("{"):
-            continue
-        j = json.loads(line)
-        timestr = j["time"].split(".")[0]
-        try:
-            dt = datetime.datetime.strptime(timestr, "%Y-%m-%dT%H:%M:%S")
-        except Exception:
-            dt = datetime.datetime.strptime(
-                timestr, "%Y-%m-%d %H:%M:%S"
-            )  # 2023-12-06 13:02:08.627029146
-        if (now - dt).total_seconds() > 60 * 60 * 24:
-            continue
-        # print(
-        #     dt,
-        #     j["boiler_modulation_rate"],
-        #     j["boiler_target_temp"],
-        #     j["output_temp"],
-        #     j["outdoor_temp"],
-        # )
-        if j["boiler_modulation_rate"] > 0:
-            if state == 0:
-                last_start = dt
-                last_stop = None
-            state = 1
-        else:
-            if state == 1:
-                last_stop = dt
-                duration_minutes = (last_stop - last_start).total_seconds() / 60.0
-                print(f"Run duration: {duration_minutes:.2f}m")
-            state = 0
-        times.append(dt)
-        tar = j["boiler_target_temp"]
-        if tar > 120:
-            tar = 0
-        targets.append(tar)
-        if "indoor_temp" in j and j["indoor_temp"] is not None:
-            temp_times.append(dt)
-            indoor_temps.append(j["indoor_temp"])
-        outside_temps.append(j["outdoor_temp"])
-        return_temps.append(j["local_return_temp"])
-        actual.append(j["output_temp"])
-        modulations.append(j["boiler_modulation_rate"])
-        if prev_time is not None:
-            btu_used += (
-                BOILER_BTU
-                * (j["boiler_modulation_rate"] / 100.0)
-                * ((dt - prev_time).total_seconds() / 3600.0)
-            )
-        prev_time = dt
-        if start_time is None:
-            start_time = dt
-        last_time = dt
+for log in ["log.0.json", "log.json"]:
+    with open(log) as f:
+        for line in f:
+            if not line.startswith("{"):
+                continue
+            j = json.loads(line)
+            timestr = j["time"].split(".")[0]
+            try:
+                dt = datetime.datetime.strptime(timestr, "%Y-%m-%dT%H:%M:%S")
+            except Exception:
+                dt = datetime.datetime.strptime(
+                    timestr, "%Y-%m-%d %H:%M:%S"
+                )  # 2023-12-06 13:02:08.627029146
+            if (now - dt).total_seconds() > 60 * 60 * 24 * args.num_days:
+                continue
+            # print(
+            #     dt,
+            #     j["boiler_modulation_rate"],
+            #     j["boiler_target_temp"],
+            #     j["output_temp"],
+            #     j["outdoor_temp"],
+            # )
+            if j["boiler_modulation_rate"] > 0:
+                if state == 0:
+                    last_start = dt
+                    last_stop = None
+                state = 1
+            else:
+                if state == 1:
+                    last_stop = dt
+                    duration_minutes = (last_stop - last_start).total_seconds() / 60.0
+                    print(f"Run duration: {duration_minutes:.2f}m")
+                state = 0
+            times.append(dt)
+            tar = j["boiler_target_temp"]
+            if tar > 120:
+                tar = 0
+            targets.append(tar)
+            if "indoor_temp" in j and j["indoor_temp"] is not None:
+                temp_times.append(dt)
+                indoor_temps.append(j["indoor_temp"])
+            outside_temps.append(j["outdoor_temp"])
+            return_temps.append(j["local_return_temp"])
+            actual.append(j["output_temp"])
+            modulations.append(j["boiler_modulation_rate"])
+            if prev_time is not None:
+                btu_used += (
+                    BOILER_BTU
+                    * (j["boiler_modulation_rate"] / 100.0)
+                    * ((dt - prev_time).total_seconds() / 3600.0)
+                )
+            prev_time = dt
+            if start_time is None:
+                start_time = dt
+            last_time = dt
 
 
 mcf_used = btu_to_mcf(btu_used)
-duration_hours = (last_time - start_time).total_seconds() / 3600.0
+if last_time is not None and last_start is not None:
+    duration_hours = (last_time - start_time).total_seconds() / 3600.0
+    last_dur = last_time - last_start
+    if last_stop is not None:
+        last_dur = last_stop - last_start
+        print(f"Duration of last run: {(last_dur).total_seconds() / 60.0:.2f}m")
+else:
+    duration_hours = 0
 print(f"MCF of gas used over {duration_hours:.2f} hours: {mcf_used:.2f}")
-last_dur = last_time - last_start
-if last_stop is not None:
-    last_dur = last_stop - last_start
-
-print(f"Duration of last run: {(last_dur).total_seconds() / 60.0:.2f}m")
 
 # Plot with mpl / plt
 
@@ -122,5 +137,6 @@ ax.set_xlabel("Time")
 ax.set_ylabel("Temperature")
 ax2.set_ylabel("Indoor Temp")
 ax.legend()
+ax2.legend()
 fig.autofmt_xdate()
 plt.show()
